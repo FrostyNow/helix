5de33801d0d3d7714169298ea88aeb36cc841ba4
Frosty <thks1018@gmail.com>
2026-03-16 21:55:03 +0900

    Fix typo

--- gamemode/config/sh_config.lua
+++ gamemode/config/sh_config.lua
@@ -42,7 +42,7 @@ ix.config.Add("maxAttributes", 100, "The maximum amount each attribute can be.",
 	category = "characters"
 })
 ix.config.Add("chatAutoFormat", true, "Whether or not to automatically capitalize and punctuate in-character text.", nil, {
-	category = "Chat"
+	category = "chat"
 })
 ix.config.Add("chatRange", 280, "The maximum distance a person's IC chat message goes to.", nil, {
 	data = {min = 10, max = 5000, decimals = 1},
